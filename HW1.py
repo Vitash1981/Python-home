b11742762c7b22283c0b576c12acc4da61ca7fc4
Vitash1981 <53339467+Vitash1981@users.noreply.github.com>
2019-12-12 13:25:38 +0200

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,10 +1,11 @@
 
+
 import sys # import class sys
 import re # import class reg exp
 from collections import Counter # import counter
 def search_words(textFile: object) -> object:
     text_dict = {} # create new dictionary
-    text = re.findall(r'\w+', textFile.read().lower()) #apply to text reg.exp
+    text = re.findall(r'\b\w+[\'\-\w+]\w+\b', textFile.read().lower()) #apply to text reg.exp
     text_dict = Counter(text) #use counter
     for i in sorted(text_dict): #i counter in sorted dict
         print(f'Word is ... "{i}" is placed in text {str(text_dict[i])} time(s) ') # print
