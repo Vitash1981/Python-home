64875d76c8afcc2abfff3f66e8236c8d9437ac75
Vitash1981 <53339467+Vitash1981@users.noreply.github.com>
2019-12-12 16:42:08 +0200

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,15 +1,19 @@
 import sys # import class sys
 import re # import class reg exp
 from collections import Counter # import counter
+
+
 def search_words(textFile: object) -> object:
     text_dict = {} # create new dictionary
-    text = re.findall(r'\b\w+[\'\-\w+]\w+\b', textFile.read().lower()) #apply to text reg.exp
+    text = re.findall(r'\b\w+[\'\-\w+]\w+\b', textFile.read().lower())
     text_dict = Counter(text) #use counter
-    for i in sorted(text_dict): #i counter in sorted dict
-        print(f'Word is ... "{i}" is placed in text {str(text_dict[i])} time(s) ') # print
+    for i in sorted(text_dict):
+        print(f'Word... "{i}" is placed in text {str(text_dict[i])} time(s) ') # print
+
 if len(sys.argv) < 2:
     with open('/python/Book.txt') as f:
-        search_words(f)
+       search_words(f)
 else:
-    with open(sys.argv[1], 'r') as f:
+   with open(sys.argv[1], 'r') as f:
         search_words(f)
+
