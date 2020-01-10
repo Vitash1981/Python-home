1d1b474db884da1d23f5f6d2d3b8a9cef37e8681
Vitash1981 <53339467+Vitash1981@users.noreply.github.com>
2020-01-10 12:57:22 +0200

    Update HW4.py
    some changes

--- HW4.py
+++ HW4.py
@@ -4,19 +4,13 @@ import argparse
 import csv
 import sys
 
-# parsing cmd line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-path_csv')
 parser.add_argument('-col_name')
 ns = parser.parse_args(sys.argv[1:])
-# print(ns.path_cvs)
-# print(ns.col_name)
-# reading column
+
 result = []
 with open(ns.path_csv) as f:
     reader = csv.DictReader(f)  # read rows into a dictionary format
-#     for row in reader:  # read a row as {column1: value1, column2: value2,...}
-#         result.append(row[ns.col_name])
-    result = [row[ns.col_name] for row in reader]
-#         print('appended ' + row[ns.col_name])
+    result = [row[ns.col_name.replace('_', ' ')] for row in reader]
 print('\n'.join(result))
